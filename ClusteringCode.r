31e6512946bdecd04cdfb94243f0af654a44dd5d
Yitaek Hwang <yitaek@gmail.com>
2016-03-30 21:43:37 -0400

    added heatmaps

--- ClusteringCode.r
+++ ClusteringCode.r
@@ -1,16 +1,46 @@
 ## Biology 311 Final Project
-# Exploring VNG0258H Gene
+# Exploring RosR - Clustering Analysis
+
+## Loading all the required packages
+library(dendextend)
+library(gplots)
 
 # Read in the Gene Expression Data
-ccdata = read.delim("GEdata.txt", header=TRUE, sep="\t", row.names=1)
+gedata = read.csv("GEdata.csv", header=T, row.names=1)
 
-# Find the VNG0258H subset
-RosR <- subset(ccdata, grepl("^VNG0258H", row.names(ccdata)))
+# Subset of the Parent Strain
+parent = t(gedata[1:8])
 
-# We want to replicate Figure 1 in the Sharma et al paper
+# Subset of the RosR Deletion
+RosRDel = t(gedata[9:16])
 
-# halo.cor <- cor(ccdata, use="pairwise.complete.obs")
-# halo.dist <-as.dist(1-halo.cor)
-# halo.tree <-hclust(halo.dist, method="complete")
+# Heatmap for the Parent Strain	
+parent.T = t(parent)
+parent.max.times <- apply(parent.T, 1, which.max)
+parent.gene.order <- rev(order(parent.max.times))
+reordered.parent <- parent.T[parent.gene.order, 1:8]
 
-# plot(halo.tree)
+png("heatmap_reordered_parent.png", width=600, height=600)
+heatmap(reordered.parent, Colv=NA, Rowv=NA, col=greenred(64), labRow=NA, cexCol=0.5)
+dev.off()
+
+# Clustering for the Parent Strain
+
+my.dist <- function(x) {as.dist(1 - cor(x, use="pairwise.complete.obs"))}
+parent.dist <- my.dist(parent)
+parent.tree <- hclust(parent.dist, method="complete")
+parent.dend <- as.dendrogram(parent.tree)
+
+png("heatmap_with_dendrogram.png", width=600, height=600)
+heatmap(parent.T, Colv=NA, Rowv=parent.dend, col=greenred(64), labRow=NA, cexCol=0.5)
+dev.off()
+
+
+colored.parent <- set(parent.dend, "branches_k_color", h=1)
+
+parent.clusters <- cutree(parent.dend, k=7)
+table(parent.clusters)
+
+# VNG0258H gene is in cluster 6
+parent.clust6 <- names(parent.clusters[parent.clusters==6])
+
